a767a68bd4eaa5be05d47ba796b68299d4324a06
AsokTamang <ashoktmg2058@gmail.com>
2025-07-12 14:11:22 -0400

    split and join

---
+++ python/index.py
@@ -0,0 +1,7 @@
+msg ='Welcome  to  Python  101: Split  and Join'
+csv = 'Eric,John,Michael,Terry,Graham'
+friends_list = ['Eric','John','Michael','Terry','Graham']
+
+print(msg.split())   #this makes the msg into an array.
+
+print('-'.join(friends_list))   #this joins the string from array to make a string but without no space.
