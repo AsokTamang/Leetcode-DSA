00b42d28b4ca9ede3177194c96071a75818032ae
AsokTamang <ashoktmg2058@gmail.com>
2025-07-12 15:15:50 -0400

    sets

--- python/index.py
+++ python/index.py
@@ -1,7 +1,32 @@
 msg ='Welcome  to  Python  101: Split  and Join'
-csv = 'Eric,John,Michael,Terry,Graham'
+csv = 'Eric,John,Michael,Terry,Graham:TerryG;Brian'
 friends_list = ['Eric','John','Michael','Terry','Graham']
+
+  #join helps us to make the array into a string
+  #split makes the string into array.
+
 
 print(msg.split())   #this makes the msg into an array.
 
 print('-'.join(friends_list))   #this joins the string from array to make a string but without no space.
+
+
+
+
+csv = 'Eric,John,Michael,Terry,Graham:TerryG;Brian'
+friends_list = ['Exercise: fill me with names']
+
+# From the list above fill a list(friends_list) properly
+# with the names of all the friends. One per "slot"
+# you may need to run same command several times
+# use print() statements to work your way through the exercise
+
+array=','.join(csv.split(';'))  #this splits the csv at the comma
+second=','.join(array.split(':'))  #this is the final array of friends_list
+final=second.split(',')
+friends_list=(':').join(friends_list).split(':')
+friends_list.clear()
+friends_list.append(second)
+print('The modified data list is: ',friends_list)
+
+
---
+++ python/sets.py
@@ -0,0 +1,24 @@
+#Sets - Exercise
+
+#1. Check if ‘Eric’ and ‘John’ exist in friends
+#2. combine or add the two sets 
+#3. Find names that are in both sets
+#4. find names that are only in friends
+#5. Show only the names who only appear in one of the lists
+#6. Create a new cars-list without duplicates
+
+friends = {'John','Michael','Terry','Eric','Graham'}
+my_friends = {'Reg','Loretta','Colin','John','Graham'}
+print('Eric' in friends and 'John' in my_friends)
+cars =['900','420','V70','911','996','V90','911','911','S','328','900']
+sameNames=friends.intersection(my_friends)
+print('The names on both sets are:',sameNames)
+total=friends.union(my_friends)
+print('The union of both sets :',total)
+
+onlyFriends=friends.difference(my_friends)  #this prints the name that are only in friends set
+onlyMy_friends=my_friends.difference(friends)
+print('Names that are  only in  my friends set',onlyMy_friends)
+print('names that are only in friends set',onlyFriends)
+print(set(cars))  #here we are converting the list into set
+
