abecb1d0c964a19d65e6338967dc301ee469d2d1
AsokTamang <ashoktmg2058@gmail.com>
2025-07-14 00:52:16 -0400

    zip and unzipped

--- python/classObjects.py
+++ python/classObjects.py
@@ -12,3 +12,29 @@ js=Coding('javascript','software development','fast and effetive')      #here we
 python=Coding('Python','Aritficial Intelligence','fast and scalable')       #same here with python
 
 Coding.description(python)   #then we are calling the method called description of class coding with var js lets see what this gonna do
+
+
+
+class Player:
+    def __init__(self,name,health,motion,rest):
+        self.name=name
+        self.health=health
+        self.motion=motion
+        self.rest=rest
+    def move(self):
+        print(f'{self.name}\'s  initial health was {self.health}')
+        self.health-=5
+        self.motion=True
+        self.rest=False
+        print(f'{self.name} moved, his health is: {self.health} ')
+class Healer(Player):
+    def healing(self):
+        self.health+=5
+        self.rest=True
+        self.motion=False
+        print(f'{self.name} rested, his health is {self.health} ')
+
+
+messi=Healer('Messi',45,True,False)   #here we are passing attributes wth 45 , true and false using class Player
+messi.move()  #then we are passing the method called move on messi 
+messi.healing()
---
+++ python/zip.py
@@ -0,0 +1,21 @@
+nums = '1234' 
+letters = ['a','b','c','d']
+names =['John','Eric','Michael','Graham','Joe']
+
+combo = zip(nums,letters,names)  #here we are zipping the datas called nums , letters and names inside a combo.
+
+
+numbers,letterss,namess=zip(*combo)  #here * means we are unzipping the data using zip
+print('The unzipped variables are: ',numbers,letterss,namess)
+
+
+keys = 'this parrot is deceased'
+values = 'denna papegojan är avliden'
+keyList=keys.split(' ')     #here we are spliting the keys at the space which convertes the sentence into an array
+valueList=values.split(' ')
+dictzipped=(dict(zip(keyList,valueList)))
+print(dictzipped)
+print('The unzipped keys and values are: ',list(dictzipped.keys()),list(dictzipped.values()))
+
+
+
