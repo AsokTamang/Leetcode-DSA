af21a3526ddfea37516a8d9cd9a2acd2f61ba006
AsokTamang <ashoktmg2058@gmail.com>
2025-07-17 20:45:33 -0400

    more patterns

--- basics/pattern.py
+++ basics/pattern.py
@@ -17,3 +17,39 @@ def pattern2(n):
         print(i * '*')
 
 pattern2(5)
+
+
+def pattern3(n):
+    for i in range(1,n+1):
+      for j in range(1,i+1):
+          print(j,end=' ')  #this code prints the pattern which runs inside the j loop in the same line to get the desired result
+
+      print()  #this line prints the pattern which runs in i loop in the next line to get the desired result    
+        
+
+pattern3(5)
+
+
+def pattern4(n):
+    for i in range(1,n+1):
+        for j in range(1,i+1):
+            print(i,end='')   #here we are using i inside the print cause for the pattern to be matched the value must be go with the outer loop
+        print()    
+
+pattern4(5)
+
+def pattern5(n):
+    for i in range(n,0,-1):
+        print('*' * i)
+
+pattern5(5) 
+
+
+
+def pattern6(n):
+    for i in range(n,0,-1):
+       for j in range(1,i+1):
+           print(j,end=' ')#here we are using the end='' inroder to print the pattern which runs inside the j loop in the same row
+       print()  #then we use this print() for printing the pattern that runs inside the i loop in the nextline 
+
+pattern6(5)
