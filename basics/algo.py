acea3d8d4d908e9b7fb96ff39a511a0b56e01e31
AsokTamang <ashoktmg2058@gmail.com>
2025-07-19 01:02:43 -0400

    algos

---
+++ basics/algo.py
@@ -0,0 +1,7 @@
+def countDigit(n):
+    print(len(str(n)))
+countDigit(15)   
+
+def reverseDigit(n):
+    print(str(n)[::-1])  #here we are first converting the digits into a string then we are reversing the order
+reverseDigit(1056)    
