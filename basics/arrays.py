c66acc80b35572eda4a221e41f0386a863050ce1
AsokTamang <ashoktmg2058@gmail.com>
2025-07-22 14:44:46 -0400

    second largest

--- basics/arrays.py
+++ basics/arrays.py
@@ -7,3 +7,39 @@ def maxelem(array):
      
     print(max)
 maxelem([5,4,10,2,1])    
+
+#Given an array of integers nums, return the second-largest element in the array. If the second-largest element does not exist, return -1.
+  
+#better approach  
+#its time complexity will be O(N) and space complexity will also be O(2) which is constant
+def secondLargest(array):
+    largest=1  #here we are just assigning the positive inf float value to the largest variable
+    seclarge=-1  #same with the seclarge variable
+    for i in range(len(array)):
+        if(array[i]> largest):
+            largest=array[i]
+    for i in range(len(array)):
+        if(array[i]>seclarge and (array[i]!=largest or array[i]<largest)):
+            seclarge=array[i]
+        elif (array[i]>seclarge and array[i]>largest):
+            largest=array[i]
+            seclarge=largest           
+    print(largest)
+    print(seclarge)
+secondLargest([10,11,4,2,1,11,9])    
+
+#optimal approach
+
+def seclrgest(array):
+    largest=1
+    seclargests=-1
+    for i in range(len(array)):
+        if array[i]>seclargests and array[i]>largest:   #here when the i index array value is greater than seclargest and also greater than the largest 
+            seclargests=largest   #then ofcourse the largest will be the seclargest and the  array[i] will be the largest
+            largest=array[i]
+             
+        elif array[i]>seclargests and (array[i]!=largest or array[i]<largest):      #and if the value is greater than the seclargest and is not equal to the largest or lesser than the largesst then 
+            seclargests=array[i]      #the sec largest will be the array[i] value
+    print(largest)
+    print(seclargests) 
+seclrgest([10,11,4,2,1,11,9])            
--- basics/quicksort.py
+++ basics/quicksort.py
@@ -1,9 +1,9 @@
 def quicksort(array):
-    if (len(array)<=1):   #if due to the recustion only one element is left in an array either left or right then we return the total array
-        return(array)
-         
-    pivot=array[0]  #here we are taking the first element of an array as the pivot
-    left = [num for num in array[1:] if num <pivot]
-    right=[num for num in array[1:] if num>=pivot]
-    return(quicksort(left) + [pivot] + quicksort(right))
-print(quicksort([7,2,1,9,11,6,8]))   
+    if(len(array)<=1):
+        return array
+    pivot=array[0]   #here in each recursion we are taking the first element of an array as the pivot
+    left=[num for num in array[1:] if num < pivot]   #then we are making the list of numbers smaller than the pivot inside a left array
+    right=[num for num in array[1:] if num >= pivot] # and in the right the numbers greater than the pivot are taken
+    return quicksort(left) + [pivot] + quicksort(right)
+sorted=(quicksort([9,7,8,1,2,7,8,2,8]))
+print('the second largest is :', sorted[len(sorted)-2])
