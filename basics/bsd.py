e834ce8597026a0a4a0e5e61b517619cabb28cb0
AsokTamang <ashoktmg2058@gmail.com>
2025-08-12 00:57:29 -0400

    optimal approach of Koko eating bananas

--- basics/bsd.py
+++ basics/bsd.py
@@ -1,3 +1,4 @@
+import math
 #Given a positive integer n. Find and return its square root. If n is not a perfect square, then return the floor value of sqrt(n).
 #brute approach
 def brutesqr(num):
@@ -67,8 +68,48 @@ print(betternth(4, 69))
 #space complexity : O(1)
       
 
-           
+#KOKO EATING BANANAS
+#A monkey is given n piles of bananas, where the 'ith' pile has nums[i] bananas. An integer h represents the total time in hours to eat all the bananas.
+#Each hour, the monkey chooses a non-empty pile of bananas and eats k bananas. If the pile contains fewer than k bananas, the monkey eats all the bananas in that pile and does not consume any more bananas in that hour.
+#Determine the minimum number of bananas the monkey must eat per hour to finish all the bananas within h hours.
+
+#brute approach
+#so the question is literally asking us to find the minimum number of bananas that a monkey must eat per hour to finish all the bananas in the given piles so that the total time taken will be exactly the given time h hrs
+#and to calculate the time for each pile we devide the pile by the number of hours and the number of hours lie between 1 and the maximum number of bananas in the given array of piles
+def brutekoko(array,h):
+    for i in range(1,max(array)+1):  # as the required minimum number of bananas lie between 1 and maximum number of banana in an array, we are using this for loop here
+        totaltime=0
+        for num in array:
+            totaltime+=math.ceil(num/i)   #here we are using math.ceil cause we are calculating the number of hours taken for koko to finish eating the bananas from the given pile ,
+        if totaltime==h:
+            ans=i
+    return ans
+print(brutekoko([25, 12, 8, 14, 19],5))            
+#time complexity : O(max(array) * N)   where N is the number of piles in an array
+# space complexity : 0            
 
 
+
+#better or optimal approach
+#in the optimal approach what we do is , we just use the binary search method between 1 and the maximum number in an array,
+#and calculate the total time taken by adding for each piles based on its number of bananas, and if the total time is equal to the given time while the number of bananas to be eaten is minimum , then we get our answer.
+def betterkoko(array,h):
+    left = 1
+    right = max(array)   #as our answer lies between 1 and the maximum number in an array
+    while left<=right:
+        mid = (left + right) //2
+        totaltime=0
+        for num in array:    #this loop runs for N times
+            totaltime+=math.ceil(num/mid)    #we are using the math.ceil cause we also need to find the extra hours taken for each pile
+        if totaltime<=h:
+            ans=mid         
+            right=mid-1 #as our answer must be the minimum number of bananas per hours for the total time to be exactly h , we decrease our range inorder to make our mid lesser or smaller which is the divider of the number of bananas in the pile  , to increase the totaltime value 
+            #so that we will have a higher chance of getting the matched total time.
+        elif totaltime>h:
+           left=mid+1
+    return ans        
+print(betterkoko( [25, 12, 8, 14, 19], 5))
+#time complexity : O(log(max(array) * N))   
+#space complexity : O(1)
 
 
