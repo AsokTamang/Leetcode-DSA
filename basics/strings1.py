617435f427da4a2442b7c7bd45c4c732c27cf091
AsokTamang <ashoktmg2058@gmail.com>
2025-08-24 22:15:55 -0400

    sort characters by frequency

---
+++ basics/strings1.py
@@ -0,0 +1,13 @@
+#Sort Characters by Frequency
+#You are given a string s. Return the array of unique characters, sorted by highest to lowest occurring characters.
+#If two or more characters have same frequency then arrange them in alphabetic order.
+def sortfreq(s):
+    m={}
+    for char in s:
+        m[char]=m.get(char,0) + 1 #here the default value will be 0 and we are adding 1 as soon as we found the occurence of letter 
+    
+    return(sorted(m,key=lambda x: m[x]))[::-1]  #here what we are doing is we are sorting our dict called m by using sorted and lambda where x:m[x] means sorting based on the values and using [::-1] to reverse the output 
+#as the question is asking us to return from highest to lowest
+print(sortfreq("tree"))
+#time complexity : O(N) + O(K)  #here N is the number of characters in a given string and K is the number of keys in our dictinary m
+#space complexity : O(N)
