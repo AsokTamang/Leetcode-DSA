7de3b6d246c0f4dda003d5ae3040604f5ec6cebe
AsokTamang <ashoktmg2058@gmail.com>
2025-08-29 15:43:59 -0400

    searching element in a linked list

--- basics/linkedlist.py
+++ basics/linkedlist.py
@@ -13,16 +13,7 @@ class LinkedList:
         node = Node(data,self.head)      #here we are creating a node having a data  
         self.head = node   #then we are creating a head based on the node we just created 
         #which means now the self.head which is the header of the linked list will be the recently created node
-    def print(self):
-        if self.head is None:
-            print('Linked list is empty')
-            return
-        iteration = self.head
-        value = ''
-        while iteration:
-            value+=str(iteration.data) + '-'  #here as we are adding the value at the beginning of the data of every current node thats why the latest value will always be inserted at the beginning
-            iteration=iteration.next
-        print(value)
+   
     def insert_at_end(self,data):  #to insert the data at the end first we need to check whether the self.head is null or not
         if self.head is None:
             self.head=Node(data,next=None)  #as we are inserting the data at the end , thats why our next will be none
@@ -31,11 +22,68 @@ class LinkedList:
         while itr:  #if the headnode of the linked list does exist then the current itr node will be the next itr node
             itr=itr.next
         #then after going through all the next nodes , we obviously come to the last or tail node
-        itr=Node(data,next=None)       #then we insert the value or data at the last of the linked list whose next is None
+        itr.next=Node(data)      
+          #then after going through each and every itr value or node we are inserting the new node at the end
+           #then we insert the value or data at the last of the linked list whose next is None
+    def insert_datalist(self,datalist):
+        self.head=None  #as we are creating a completely new linked list , so we can use either one of the insert at the end or insert at the beginning function to insert these datas
+        for data in  datalist:
+            self.insert_at_the_beginning(data)
+        itr= self.head
+        c=0   
+        while itr:   #this loop is for counting the number of nodes in the linked list
+            c+=1
+            itr=itr.next
+        return c    
+    
+    def remove_at(self,pos):
+        itr=self.head   #here head will always be the head of the node
+        c = 0
+        if pos==0:  #here if we want to remove the first element then the self.head will be the self.head.next
+            self.head=self.head.next 
+        if pos<0 or pos>=self.countlength():  #here the index must lie with in the range of our linked list length
+            raise Exception('Invalid index')     #if the target index lies below 0 or is greater than or equal to the length of the linked list then we just raise an invalid index
 
+        while itr:
+            if c==pos - 1: #suppose we need to remove the element at index 1 then of course at first the count will be 0
+                itr.next=itr.next.next  #here to remove the element at index 1 , the count must be 0 which is only possible when the current itr will be the first number 
+                #so itr.next which is the 1 indexed number that we want to remove = itr.next.next will automatically remove the current itr.
+                break
+            c+=1
+            itr=itr.next
+        value = ''
+        while itr:
+            value+=itr.data
+        return value    
+
+
+
+    def countlength(self):
+        itr=self.head
+        c=0
+        while itr:
+            c+=1
+            itr=itr.next   #as long as the itr or the node exists then we just keep on counting and going over the next node only after increasing the count by 1
+        return(c)
+
+
+
+    def print(self):
+        if self.head is None:
+            print('Linked list is empty')
+            return
+        iteration = self.head
+        value = ''
+        while iteration:   #as long as the head or the node exists then we just keep on printing the values in the linked list
+            value+=str(iteration.data)+ ', '  #here as we are adding the value at the beginning of the data of every current node thats why the latest value will always be inserted at the beginning
+            iteration=iteration.next
+        print(value)
 
 if __name__=='__main__':
     ll=LinkedList()
     ll.insert_at_the_beginning(11)
     ll.insert_at_the_beginning(7)
-    ll.print()        
+    ll.insert_datalist(['apple','banana','orange'])
+    ll.countlength()
+    ll.print()
+    print(ll.remove_at(1))  
---
+++ basics/prac.py
@@ -0,0 +1,86 @@
+class Node:  #here we are making a class of node ,
+    #this class is responsible for creating a node
+    def __init__(self,data=None,next=None):
+        self.data=data
+        self.next=next
+
+class LinkedList:
+    #this class is mainly responsible for creating the linked list and the linked list always has the head. so , we must create a head of this linked list
+    def __init__(self):  
+        self.head=None
+    def insert_at_beginning(self,data):  #here to insert the new data at the beginning of the linked list , we first need to create a node having its next as the self.head which is the previous head of the linked list
+        value = Node(data,self.head)
+        self.head=value        #here self.head means we are creating a new head which is this newly created node
+    def insert_at_end(self,data):
+        itr=self.head  #here we are storing the head of the linked list in a var called itr
+        while itr.next:  #then as long as this itr.next exists which we try to avoid or move
+            itr=itr.next     #by making it's value the next node from it
+        #Then at last we come to the node where the itr.next does not exists then this itr's next will be the newly created node that we have.    
+        new_value=Node(data,None)  #as at the last node the next will be none   
+        itr.next=new_value    #in this way, we insert the value at the last of the linked list
+    def print(self):
+        itr = self.head  
+        value=''
+        while itr:
+            value+=str(itr.data)+','
+            itr=itr.next
+        return value
+    def countlength(self):
+        itr = self.head
+        count = 0
+        while itr:
+            count+=1
+            itr=itr.next
+        return count
+    def searchelem(self,pos):
+        itr = self.head
+        count = 0
+        value = ''
+        if pos==0:
+            value+=self.head.data
+            return value
+        if pos<0 or pos>=self.countlength():
+            raise Exception('Invalid index')
+        while itr:
+            if count==pos-1:
+                value+=itr.next.data  #here the count must be pos-1 , then it's next node data will be the required data that is needed to search
+                return value
+
+
+           
+            itr=itr.next
+            count+=1
+
+        
+
+    def removethenode(self,pos):
+        count = 0
+        itr = self.head
+        if pos == 0:  #if we want to remove the first or the head of the linked list then the self.head will be changed into self.head.next
+            self.head=self.head.next
+            return
+        if pos<0 or pos>=self.countlength():
+            raise Exception('Invalid index')  #if the targeted index is lesser than 0 and greater than or equal to the length of the linked list then we raise an exceptional error.
+        while itr:
+            if count == pos - 1:   #here this is like a trick or you can say a formulae ,if the current count is same as that of the index-1 then we come to the targeted itr or head whose next is the head we want to remove
+                itr.next=itr.next.next
+                break
+            count+=1
+            itr=itr.next
+            
+
+
+        
+
+
+
+
+if __name__=='__main__':
+    ll=LinkedList()
+    ll.insert_at_beginning('77')
+    ll.insert_at_end('11')
+    ll.insert_at_end('22')
+    print(ll.searchelem(1))
+    print(ll.countlength())
+    print(ll.print())
+    
