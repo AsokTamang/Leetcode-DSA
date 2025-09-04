86fdfb5b577771a1892d60a0a4ced901aece8d85
AsokTamang <ashoktmg2058@gmail.com>
2025-09-04 13:46:25 -0400

    optimal approach of checking palindrome

--- basics/pal.py
+++ basics/pal.py
@@ -20,8 +20,34 @@ class llist:
         return False    
     #time complexity : O(N)
     #space complexity : O(N)
+    def optimalpalindrome(self):
+        slow =self.head
+        fast=self.head
+        while fast and fast.next and fast.next.next:
+            slow=slow.next
+            fast=fast.next.next
+        m1=slow  #here m1 is the last node of the first half of the linked list
+        m2=slow.next  #and m2 is the last node of the second half of the linked list
+        prev=None
+        while m2:
+            front = m2.next
+            m2.next=prev
+            prev=m2
+            m2=front
+        itr=self.head    
+        while prev:
+            if itr.data!=prev.data:  #if any of the datas at the corresponding positions of the splitted half of the linked list,  are not same then we just return false
+                return False
+            itr=itr.next
+            prev=prev.next
+        return True    
+    #time complexity : O(N)
+    #space complexity : O(1)
+
+
+
 a=llist()
 a.head=Node(1,None)
 a.head.next=Node(1,None)
-a.head.next.next=Node(1,None)
-print(a.brutecheckpalindrome())
+a.head.next.next=Node(3,None)
+print(a.optimalpalindrome())
