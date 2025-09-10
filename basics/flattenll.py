3a3787e824bf57b390d81da93c59046049cce1d5
AsokTamang <ashoktmg2058@gmail.com>
2025-09-10 18:56:23 -0400

    optimal approach of flattening the linked list

--- basics/flattenll.py
+++ basics/flattenll.py
@@ -26,17 +26,48 @@ class flattenlist:
         self.head=Node(a[0],None,None)
         itr=self.head
         for data in a[1:]:
-            itr.next=Node(data,None,None)
-            itr=itr.next
+            itr.child=Node(data,None,None)
+            itr=itr.child
         return self.head  
     #time complexity : O(logN*C+NC)
     #space complexity : O(N*C)  
+    def mergelist(self,l1,l2):
+        dummynode=Node(-1,0,0)
+        temp=dummynode
+        while l1 and l2:
+            if l1.data<l2.data:
+                temp.child=l1
+                temp=temp.child
+                l1=l1.child
+            else:
+                temp.child=l2
+                temp=temp.child
+                l2=l2.child
+           
+        if l1:
+            temp.child=l1  #as our child nodes are already sorted , we just need to do temp.child = l1
+        else:
+            temp.child=l2 
+        self.head=dummynode.child           
+        return self.head  #the next of the dummynode will be the head            
+
+    def optimalflatten(self,head):
+        if head is None or head.next is None:
+            return head
+        #when the base case is hit or the last node is reached then this returned head will be the next of the head of the previous callstack
+        #then we merge the head and the head.next
+        head.next=self.optimalflatten(head.next)  #this code uses the recursion method to go deep into the  linked nodes till the base case is hit
+        return self.mergelist(head,head.next)
+            
+
+           
+
     def printdatas(self):
         itr=self.head
         v=[]
         while itr:
             v.append(str(itr.data))
-            itr=itr.next
+            itr=itr.child
         return ','.join(v) 
 
 
