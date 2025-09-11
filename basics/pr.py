545136ae4b916f6c0dcf0344a6d3f87d6f8524e0
AsokTamang <ashoktmg2058@gmail.com>
2025-09-11 13:39:19 -0400

    brute approach of cloning a linked list with random and next pointer

--- basics/pr.py
+++ basics/pr.py
@@ -1,65 +1,50 @@
+#Clone a LL with random and next pointer
+#so the question is asking us to clone a linked list which has a next pointer that points to its next node and the random pointer which points to its random nodes or even null or none too
+
 class Node:
-    def __init__(self,data,child,next):
+    def __init__(self,data,next,random):
         self.data=data
-        self.child=child
         self.next=next
+        self.random=random
     def __str__(self):
-        return str(self.data)
+        return self.data
 class linkedlist:
-    def mergerll(self,l1,l2):
-        dummynode=Node(-1,None,None)
-        temp=dummynode
-        while l1 and l2:  #here we are taking the two nodes and try to merge them in the sorted order, based on the datas between them and we go on to the child of the nodes 
-            if l1.data<l2.data:
-                temp.child=l1
-                temp=temp.child
-                l1=l1.child
-            else:
-                temp.child=l2
-                temp=temp.child
-                l2=l2.child
-        if l1:
-            temp.child=l1   
-        else:
-            temp.child=l2     
-        self.head=dummynode.child
-        return self.head
-
-
-    def flattenll(self,head):
-        if head is None or head.next is None:  
-            return head
-        head.next=self.flattenll(head.next)
-        return self.mergerll(head,head.next)
-    #time complexity : O(N*C)
-    #space complexity : O(1)
-    def printdatas(self):
+    def __init__(self):
+        self.head=None
+    def clonell(self):
+        m={}  #this is our map dictionary
         itr=self.head
-        v=[]
         while itr:
-            v.append(str(itr.data))
-            itr=itr.child
-        return ','.join(v)
-a=linkedlist()
-a.head=Node(3,None,None)  #next comes first and child comes second
+            m[itr]=Node(itr.data,None,None)  #here we are copying the node from the given or original linked list with the node and value as key value pair            
+            itr=itr.next
+        itr=self.head
+        while itr:
+            m[itr].next=m[itr.next]
+            m[itr].random=m[itr.random]
+            itr=itr.next
+        self.head=m[self.head]   #as we have already copied the self.head in our dictionary, we are using m[self.head]
+        return self.head  
+    #time complexity : O(N)
+    #space complexity : O(N)
+    def printdatas(self):   
+        itr=self.head
+        a=[]
+        while itr:
+            randomvalue=itr.random.data if itr.random else -1  #as the question has stated that if the node doesnot has a random pointer then its random data will be -1
+            a.append(str(randomvalue)) 
+            itr=itr.next
+        return ','.join(a)    
+a=linkedlist() 
+a.head=Node(1,None,None)
 a.head.next=Node(2,None,None)
-a.head.next.next=Node(1,None,None)
+a.head.next.next=Node(3,None,None)
 a.head.next.next.next=Node(4,None,None)
 a.head.next.next.next.next=Node(5,None,None)
-a.head.child=None
-a.head.next.child=Node(10,None,None)
-a.head.next.child.child=None
-a.head.next.next.child=Node(7,None,None)
-a.head.next.next.child.child=Node(11,None,None)
-a.head.next.next.child.child.child=Node(12,None,None)
-a.head.next.next.child.child.child.child=None
-a.head.next.next.next.child=Node(9,None,None)
-a.head.next.next.next.child.child=None
-a.head.next.next.next.next.child=Node(6,None,None)
-a.head.next.next.next.next.child.child=Node(8,None,None)
-a.head.next.next.next.next.child.child.child=None
-a.flattenll(a.head)
+a.head.random=None
+a.head.next.random=a.head
+a.head.next.next.random=a.head.next.next.next.next
+a.head.next.next.next.random=a.head.next
+a.head.next.next.next.next.random=a.head.next.next
 print(a.printdatas())
-        
 
-        
+           
