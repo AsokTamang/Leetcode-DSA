57fd36bd645e8e810debd8d166bfea081156eda5
AsokTamang <ashoktmg2058@gmail.com>
2025-09-14 20:58:31 -0400

    revision of sorting a stack recursively

--- basics/algo.py
+++ basics/algo.py
@@ -71,3 +71,27 @@ def prime(n):
     else:
         print('Composite number')       
 prime(11)                
+
+
+def sortastack(array):
+    if len(array)==1:
+        return array
+    else:
+        if not array or len(array)==1:  #and our base case is that if only one number remains inside an array we return 
+            return 
+        top=array.pop()  #we keep on deleting the last element from our array till only one number remains inside an array.
+        sortastack(array) #by calling this same function recursively
+        insertelement(array,top)     #then after the base case is reached, we insert those popped numbers recursively using another function
+        return array
+def insertelement(array,element):
+    if not array or element<=array[-1]:  #as the question is asking us to sort the array in descending order , thats the passed element must be lesser than the number that is already in an array
+        #and in some cases if the only remaining number in an array is lesser than the passed number then we have to use the condition of if not array.
+        array.append(element)
+        return    
+    else:
+        top=array.pop()  #if the number which is already inside an array is lesser than the passed element then we pop this number then try inserting the passed number recursivley
+        insertelement(array,element)
+        array.append(top) #and at last , we insert that deleted number
+print(sortastack([1, 2, 5, 10]))
+#time complexity : O(N^2)
+#space complexity : O(N)
