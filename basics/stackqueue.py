4d418801d88d2fb8d527c7da51eba1f92d9295f5
AsokTamang <ashoktmg2058@gmail.com>
2025-09-29 14:37:25 -0400

    queue implementation using arrays

--- basics/stackqueue.py
+++ basics/stackqueue.py
@@ -57,14 +57,84 @@ class Stack:
             a.append(num)
         return a    
             
-s=Stack(4,0)
-(s.push(5))
-(s.push(4))
-(s.push(3))
-(s.push(2))
-s.pop()
-print(s.peek())
-print(s.peek())
+
 
 #time complexity : O(1)
 #space complexity : O(assigned size of the stack)
+
+
+
+#now for queue which follows the rule of first in first out
+#where the start indexed data will be change most of the time rather than the end index
+class Queue:
+    def __init__(self,size): 
+        self.size = size
+        self.currsize = 0  #initial length of the queue
+        self.start = -1
+        self.end = -1
+        self.stack = [None] * size
+    def isempty(self):
+        return self.currsize == 0
+    def isfull(self):
+        return self.currsize == self.size  #if the current size is equal to the fixed length of the array then our queuse is full.
+            
+    def push(self,x):
+        if self.isfull(): #if our end is at the last index then we cannot push the datas further
+            return 'queue overflow'
+        if self.currsize == 0:
+            self.start=0
+            self.end=0
+            self.stack[self.end] =x
+            self.currsize+=1
+        else:
+            self.end=(self.end+1) % self.size
+            self.stack[self.end] =x
+            self.currsize+=1
+        return self.stack[self.end]
+        
+        
+
+
+    def pop(self):
+        if self.isempty():
+            return 'no any datas left in the queue'
+        if self.currsize == 1:  #if we are at the very first index  and the current size is 1 then we just reset everything
+            self.stack[self.start]=None
+            self.currsize-=1
+            self.start-=1
+            self.end-=1
+        else:
+            self.stack[self.start]=None
+            self.currsize-=1
+            self.start=(self.start+1) % self.size   
+
+    def peek(self):
+        if self.currsize == 0:
+            return 'no any datas left in the queue'
+        return self.stack[self.start]  #here we always represent the top most data which is the first data inserted in the queue
+    def display(self):
+        a= [ ]
+        for num in self.stack:
+            a.append(num)
+        return a
+q=Queue(4)
+(q.push(1))
+(q.push(2))
+(q.push(3))
+(q.push(4))
+q.pop()
+q.pop()
+q.pop()
+(q.push(4))
+(q.push(4))
+(q.push(4))
+print(q.push(4))
+
+print(q.display())
+#time complexity : O(1)
+#space complexity : O(Q)  size of the queue
+
+    
+    
+
+    
