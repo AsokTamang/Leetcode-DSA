73fe46dc51c46180cdbfe4d42df493d925e4ea4f
AsokTamang <ashoktmg2058@gmail.com>
2025-10-05 21:43:24 -0400

    optimal next greater element

--- basics/stackqueuevvi.py
+++ basics/stackqueuevvi.py
@@ -13,3 +13,22 @@ def nextgreaterelement(arr):
 print(nextgreaterelement([6, 8, 0, 1, 3])) 
 #time complexity : O(N^2)
 #space complexity : O(N)        
+
+
+#optimal solution
+def optimalnext(arr):
+    stack = []   #this stores the very next numbers greater than the corresponding number at the current iteration
+    ans = [-1] * len(arr)   #making a ans variable which stores the answer
+    for i in range(len(arr)-1,-1,-1):  #looping from the last index of the given array
+        while stack and stack[-1]<=arr[i]:  #for the very first loop or lets say the very last indexed number this while loop won't run casue there are no numbers or values in the stack and also ,
+            #the next greater number for this last indexed number will always be -1
+            stack.pop()
+
+        
+        if stack:  #if there is still numbers or values in the stack which means we have found a number greater than the i indexed number just very close to it.
+            ans[i] = stack[-1]  #the last value will be the next greater number for this i indexed number    
+        stack.append(arr[i])  #appending the i indexed number from array in the stack 
+    return ans
+print(optimalnext([6, 8, 0, 1, 3]))
+#time complexity : O(N)
+#space complexity : O(N)
