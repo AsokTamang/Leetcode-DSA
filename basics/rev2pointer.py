60701bdecb65d063f3b33f9583d86446b0e23587
AsokTamang <ashoktmg2058@gmail.com>
2025-10-14 20:05:54 -0400

    revision of optimal longest susbtring without repeating characters

--- basics/rev2pointer.py
+++ basics/rev2pointer.py
@@ -12,4 +12,26 @@ def longestsubstring(s):
     return maxlength
 print(longestsubstring("aaabbbccc"))   
 #time complexity : O(N**2)
-#space complexity : O(1)      
+#space complexity : O(1)     
+
+#optimal approach
+def optimallongestsubstring(s):
+    l=r=0
+    n=len(s)
+    m={}  #here we are storing the character and its corresponding index as key-value pair
+    maxlength = 0
+    while r<n:
+        if s[r] in m and m[s[r]]>=l:
+            l=m[s[r]] + 1
+        m[s[r]] = r
+        maxlength=max(maxlength,r-l+1)
+        r+=1
+    return maxlength
+print(optimallongestsubstring("abcddabac"))    
+#time complexity : O(N)
+#space complexity : O(M) number of unique characters in the given string  
+
+
+
+
+ 
