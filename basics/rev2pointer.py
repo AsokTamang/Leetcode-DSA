ab4007f3265d4e1727433e13eec92ecbb675640e
AsokTamang <ashoktmg2058@gmail.com>
2025-10-15 12:05:54 -0400

    revision of brute approach of length of longest substring Without Repeating Characters

--- basics/rev2pointer.py
+++ basics/rev2pointer.py
@@ -1,37 +1,16 @@
-#longest substring without repeating characters
-def longestsubstring(s):
-    n=len(s)
+#Longest Substring Without Repeating Characters
+#Given a string, S. Find the length of the longest substring without repeating characters.
+def brutelongestsubstring(s):
     maxlength = 0
+    n=len(s)
     for i in range(n):
-        seen = set()
+        m=set()
         for j in range(i,n):
-            if s[j] in seen:
+            if s[j] in m:
                 break
-            seen.add(s[j])
-            maxlength=max(maxlength,j-i+1)
+            m.add(s[j])
+            maxlength=max(maxlength,j-i+1)  
     return maxlength
-print(longestsubstring("aaabbbccc"))   
+print(brutelongestsubstring("aaabbbccc"))        
 #time complexity : O(N**2)
-#space complexity : O(1)     
-
-#optimal approach
-def optimallongestsubstring(s):
-    l=r=0
-    n=len(s)
-    m={}  #here we are storing the character and its corresponding index as key-value pair
-    maxlength = 0
-    while r<n:
-        if s[r] in m and m[s[r]]>=l:
-            l=m[s[r]] + 1
-        m[s[r]] = r
-        maxlength=max(maxlength,r-l+1)
-        r+=1
-    return maxlength
-print(optimallongestsubstring("abcddabac"))    
-#time complexity : O(N)
-#space complexity : O(M) number of unique characters in the given string  
-
-
-
-
- 
+#space complexity : O(M)  M is the number of unique characters in the substring
