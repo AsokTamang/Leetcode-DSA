d31c3164510087f9ce76113177aae3fb67782183
AsokTamang <ashoktmg2058@gmail.com>
2025-10-15 12:58:23 -0400

    brute Longest Repeating Character Replacement

--- basics/rev2pointer.py
+++ basics/rev2pointer.py
@@ -147,6 +147,22 @@ print(optimalfruitintobaskets([1, 2, 3, 2, 2]))
 #space complexity : O(1)
 
 
+#Longest Repeating Character Replacement
+def brutelongestrepchar(s,k):
+    n=len(s)
+    maxlength = 0
+    for i in range(n):
+        count = 0
+        for j in range(i,n):
+            if s[j] == 'B':
+                count+=1
+            if count>k:
+                break
+            maxlength = max(maxlength,j-i+1)  #the function will never reach this code if the total count becomes greater than k
+    return maxlength
+print(brutelongestrepchar("AABABBA" , k = 1))   
+#time complexity : O(N**2)
+#space complexity : O(1)      
 
 
 
