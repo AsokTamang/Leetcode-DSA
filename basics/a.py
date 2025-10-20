b1616ffd566943915077f2e1e33edd19415d20c6
AsokTamang <ashoktmg2058@gmail.com>
2025-10-20 12:06:31 -0400

    rev 2

--- basics/a.py
+++ basics/a.py
@@ -1,3 +1,4 @@
+#Longest Substring With At Most K Distinct Characters
 def brutelongestsubstring(s,k):
     n=len(s)
     maxlength=float('-inf')
@@ -12,3 +13,23 @@ def brutelongestsubstring(s,k):
 print(brutelongestsubstring("abcddefg" ,3))  
 #time complexity : O(N**2)
 #space complexity : O(1)
+
+#optimal solution
+def optimalongestsubstring(s,k):
+    n=len(s)
+    l=r=0
+    m={}
+    maxlength = float('-inf')
+    while r<n:
+        m[s[r]]=m.get(s[r],0)+1
+        if len(m) > k:
+            m[s[l]]-=1
+            if m[s[l]]==0:
+                del m[s[l]]
+            l+=1
+        if len(m)<=k:        
+         maxlength=max(maxlength,r-l+1)
+         r+=1
+    return maxlength
+print(optimalongestsubstring("aababbcaacc" , k = 2))     
+        
