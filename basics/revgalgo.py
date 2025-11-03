bc75802000a3d05e7a932cdbf05059534fc6c815
AsokTamang <ashoktmg2058@gmail.com>
2025-11-03 12:56:50 -0500

    rev greedy 2

--- basics/revgalgo.py
+++ basics/revgalgo.py
@@ -16,3 +16,22 @@ def assigncookies(student,cookie):
 print(assigncookies([4, 5, 1] , [6, 4, 2]))
 #time complexity : O(NlogN+MlogM)
 #space complexity : O(1)
+
+
+#Fractional Knapsack
+def fractionalknapsack(val,wt,k):
+    totalwt =0 
+    array = sorted(zip(val,wt),key=lambda x:x[0]/x[1],reverse=True )  #here we are sorting reversely based on the fraction of value and weight
+    maxvalue =0
+    for val,wt in array:
+        if totalwt+wt<=k:
+            totalwt+=wt
+            maxvalue+=val
+        else:
+            remainingwt = k-totalwt
+            additionalval = (val/wt) * remainingwt
+            maxvalue+=additionalval
+    return f'{maxvalue:.6f}'
+print(fractionalknapsack([60,100,120],  [10,20,30],  50)) 
+#time complexity : O(NlogN)
+#space complexity : O(N)  
