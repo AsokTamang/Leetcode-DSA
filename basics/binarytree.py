22292f38daed9d2d35e0df1bbe9b02bcd47dfbb1
AsokTamang <ashoktmg2058@gmail.com>
2025-11-10 11:35:48 -0500

    Preorder traversal of a binary tree

--- basics/binarytree.py
+++ basics/binarytree.py
@@ -18,3 +18,44 @@
 #in the postorder traversal, the root node will come after the left and right binary subtree
 
 
+#Preorder Traversal
+#Given root of binary tree, return the preorder traversal of the binary tree.
+class Solution:
+    class Node:
+        def __init__(self,data=0,left=None,right=None):
+         self.data=data
+         self.left=left
+         self.right=right
+    def __init__(self):
+       pass
+    def buildtree(self,array,i=0):  #this function is for building the binary tree
+       if i>=len(array) or array[i] is None:
+          return 
+       root = self.Node(array[i])  #here we are creating a node with the value of element at index i of the given array
+       root.left=self.buildtree(array,2*i+1)
+       root.right = self.buildtree(array,2*i+2)
+       return root
+    
+    def preorder(self,root,arr):
+        if root is None:
+           return
+        arr.append(root.data)
+        self.preorder(root.left,arr)  #here we are recursively going to the left children node or lets say left binary subtree
+        self.preorder(root.right,arr)  #here we are recurvisly going to the right binary subtree
+        return arr
+s=Solution()
+r=s.buildtree( [5, 1, 2, 8, None, 4, 5, None, 6])  #here we are storing the root of the designed binary tree
+print(s.preorder(r,[]))
+#time complexity : O(N)
+#space complexity : O(N)
+
+
+
+
+    
+
+
+       
+    
+     
+
