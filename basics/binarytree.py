1c8f8a424d5a5ddaf7ed9e2f4b6c1073d939a5cf
AsokTamang <ashoktmg2058@gmail.com>
2025-11-10 11:05:36 -0500

    types of  binary tree and methods of search in binary tree

--- basics/binarytree.py
+++ basics/binarytree.py
@@ -4,3 +4,17 @@
 #balanced binary tree is a binary tree whose maximum height is logN where N is the total number of nodes in a binary tree.
 
 
+
+
+#so in a binary tree , there are two ways to solve a problem and they are :
+#BFS - breadth first search , in this search we go through every levels of the binary tree or every breadths of the binary tree
+#DFS -depth first search , in this search we go deeper into the depth of the binary tree
+#for DFS there are 3 types and they are:
+#Inorder traversal
+#in the inorder traversal the root node will be in the middle of left and right binary subtree
+#Preorder traversal 
+#in the preorder traversal , the root node will come before the left and right binary subtree  
+#postorder traversals 
+#in the postorder traversal, the root node will come after the left and right binary subtree
+
+
