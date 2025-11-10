9198692dddcf55c86b8565b45be86390c90f4099
AsokTamang <ashoktmg2058@gmail.com>
2025-11-10 10:45:41 -0500

    Intro to  binary tree

---
+++ basics/binarytree.py
@@ -0,0 +1,6 @@
+#for full a binary tree, all the nodes in a tree must have either 0 node or 2 nodes which are left and right children nodes for a parent node
+#complete binary tree is a binary tree whose last level is either full or the children nodes are in the left hand side
+#perfect binary tree is a binary tree whose last levels or the last children nodes are in the same level
+#balanced binary tree is a binary tree whose maximum height is logN where N is the total number of nodes in a binary tree.
+
+
